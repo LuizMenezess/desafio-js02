7acb5e8c6551b56c2d82cd6fc41bc203afe3b92d
LuizMenezess <luizmenezes334@gmail.com>
2021-05-13 22:04:04 -0300

    adicionando desafio js-02

---
+++ script.js
@@ -0,0 +1,21 @@
+let idade = 19
+
+if (idade > 18){
+    console.log (`Voce é maior de 18 anos`)
+}
+
+
+
+let old = 24
+
+if (old >= 18){
+    console.log (`Bem vindo`)
+}
+
+
+
+let mes = "janeiro"
+
+if ((mes === "dezembro") || (mes === "janeiro")){
+    console.log (`true`)
+}
